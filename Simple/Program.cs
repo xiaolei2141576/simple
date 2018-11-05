bf570cbdf6352b898149176b14742e9fdb7d1749
钟垒 <zl@myweiju.com>
2018-11-05 14:19:39 +0800

    修改项目名称
    Signed-off-by: 钟垒 <zl@myweiju.com>

--- Simple/Program.cs
+++ Simple/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Simple
+namespace Simple.Api
 {
     public class Program
     {
