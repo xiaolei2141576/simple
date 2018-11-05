a2278877042e69ed62a8acd5738412790f0cd610
钟垒 <zl@myweiju.com>
2018-11-05 14:43:58 +0800

    修改api项目名称

---
+++ Simple.Models/Common/TokenModelJWT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Simple.Models.Common
+{
+    public class TokenModelJWT
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public long Uid { get; set; }
+        /// <summary>
+        /// 角色
+        /// </summary>
+        public string Role { get; set; }
+        /// <summary>
+        /// 职能
+        /// </summary>
+        public string Work { get; set; }
+    }
+}
---
+++ Simple/AuthHelper/JwtTokenAuth.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Simple.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Simple.Api.AuthHelper
+{
+    public class JwtTokenAuth
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private readonly RequestDelegate _next;
+
+        public JwtTokenAuth(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public Task Invoke(HttpContext httpContext)
+        {
+            //检测是否包含'Authorization'请求头
+            if (!httpContext.Request.Headers.ContainsKey("Authorization"))
+            {
+                return _next(httpContext);
+            }
+            var tokenHeader = httpContext.Request.Headers["Authorization"].ToString();
+
+            TokenModelJWT tm = JwtHelper.SerializeJWT(tokenHeader);//序列化token，获取授权
+
+            //授权
+            var claimList = new List<Claim>();
+            var claim = new Claim(ClaimTypes.Role, tm.Role);
+            claimList.Add(claim);
+            var identity = new ClaimsIdentity(claimList);
+            var principal = new ClaimsPrincipal(identity);
+            httpContext.User = principal;
+
+            return _next(httpContext);
+        }
+    }
+}
